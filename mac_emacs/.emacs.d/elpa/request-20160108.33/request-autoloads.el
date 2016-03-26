e395e740a8100bc1b5c798eea49dca68cbcb245a
Clayton William <clayton.calvin@gmail.com>
2016-03-26 19:36:04 -0400

    remove redundant files

--- mac_emacs/.emacs.d/elpa/request-20160108.33/request-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; request-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("request.el") (22229 26883 94820 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; request-autoloads.el ends here
