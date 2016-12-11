f513f2a5f098fb7b2ba3f927a6031ec0c2b7a43e
Clayton William <clayton.calvin@gmail.com>
2016-12-11 00:02:28 -0500

    some bug fixes

--- mac_emacs/emacs.d/init.el
+++ mac_emacs/emacs.d/init.el
@@ -1,7 +1,7 @@
 ;; init.el --- Emacs configuration
 
 ;; MAC Command Key as M
-;; ----------------------------------------------------------------------
+;; -------------------------------------------------------------------
 
 ;; Configuring Emacs for OS X
 (defun system-is-mac ()
@@ -10,10 +10,11 @@
 
 ;; command key to be meta instead of option
 (if (system-is-mac)
-   (setq ns-command-modifier 'meta))
+    (setq ns-command-modifier 'meta))
+
 
 ;; INSTALL PACKAGES
-;; ----------------------------------------------------------------------
+;; -------------------------------------------------------------------
 
 (require 'package)
 
@@ -28,15 +29,17 @@
   '(better-defaults                     ; add the better-defaults
     elpy                                ; add the elpy package
     ein                                 ; add Emacs Ipython Notebook package
-    ;; autopair                            ; add autopair package
+    ;; autopair                         ; add autopair package
     smartparens                         ; add smartparens
     flycheck                            ; add flycheck (dynamic syntax check)
     auto-complete                       ; add auto-completion
     fill-column-indicator               ; add fill-column-indicator
     py-autopep8                         ; change to pep8 rules when saving .py
-    ;; material-theme                   ; add material theme
+    org                                 ; add org mode
+    material-theme                      ; add material theme
     ;; hc-zenburn-theme                 ; add zenburn-theme
-    atom-dark-theme))                   ; add atom-dark
+    ;; atom-dark-theme                  ; add atom-dark theme
+    ))
 
 (mapc #'(lambda (package)
     (unless (package-installed-p package)
@@ -49,6 +52,7 @@
 ;; (autopair-global-mode) ;; enable autopair in all buffers
 (require 'fill-column-indicator)
 (define-globalized-minor-mode global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(setq-default fci-rule-column 79)
 (global-fci-mode t)
 (ac-config-default)                     ; enable auto-completion
 (require 'smartparens-config)
@@ -65,10 +69,10 @@
 
 
 ;; BASIC CUSTOMIZATION
-;; ----------------------------------------------------------------------
+;; -------------------------------------------------------------------
 
 (setq inhibit-startup-message t) ;; hide the startup message
-(load-theme 'atom-dark t) ;; load hc-zenburn theme
+(load-theme 'material t) ;; load hc-zenburn theme
 (global-linum-mode t) ;; enable line numbers globally
 
 ;; set the default (start) directory
@@ -78,13 +82,24 @@
 
 
 ;; CPERL CUSTOMIZATION FOR PERL
-;; ----------------------------------------------------------------------
+;; -------------------------------------------------------------------
 
 (defalias 'perl-mode 'cperl-mode)
-(setq cperl-electric-keywords t)        ; expands keywords such as foreach, while
+(setq cperl-electric-keywords t)       ; expands keywords such as
+                                       ; foreach, while
 (setq cperl-indent-level 4
       cperl-tab-always-indent t
       cperl-electric-parens nil)
-(setq cperl-hairy nil)                    ; turn on lots of cperl options
+(setq cperl-hairy nil)                 ; turn on lots of cperl options
+
+
+;; ORG MODE CUSTOMIZATION
+;; -------------------------------------------------------------------
+(require 'org)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
 
 ;; init.el ends here
